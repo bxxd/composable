c1115cc4137ab3c6fdab798064d5b6f2a300772e
bxxd <bxxd.eth@gmail.com>
2023-09-23 08:17:59 -0400

    update

--- app/composable/src/app/hub/page.tsx
+++ app/composable/src/app/hub/page.tsx
@@ -24,51 +24,43 @@ export default function Page() {
   }, []);
 
   const router = useRouter(); // Initialize the router
-
   return (
     <Layout>
       <div className="App flex flex-col gap-4 mr-4 ml-2 mb-2 mt-2">
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
           {items.map((item, index) => (
-            // Display each item in a separate card
             <div
               key={index}
-              className="card rounded-lg shadow-lg cursor-pointer border"
-              onClick={() => router.push(`/ai-created/${item.id}`)} // Navigate to the new URL on click
+              className="card rounded-lg shadow-lg cursor-pointer border bg-white dark:bg-black"
+              onClick={() => router.push(`/ai-created/${item.id}`)}
             >
               <div className="pointer-events-none">
                 <Published id={item.id} />
               </div>
             </div>
           ))}
-          <style jsx>{`
-            .card {
-              position: relative;
-              overflow: hidden;
-              min-height: 20rem;
-              max-height: 20rem;
-              box-sizing: border-box; /* To include padding in width and height */
-              background-color: white; /* Setting background color */
-            }
-            .dark .card {
-              background-color: black; /* Setting background color for dark theme */
-            }
-
-            .card::after {
-              content: "";
-              position: absolute;
-              bottom: 0;
-              height: 50%;
-              left: 0;
-              right: 0;
-              background: linear-gradient(transparent, white);
-            }
-            .dark .card::after {
-              background: linear-gradient(transparent, black);
-            }
-          `}</style>
         </div>
       </div>
+
+      <style jsx>{`
+        .card {
+          position: relative;
+          overflow: hidden;
+          min-height: 20rem;
+          max-height: 20rem;
+          box-sizing: border-box;
+        }
+
+        .card::after {
+          content: "";
+          position: absolute;
+          bottom: 0;
+          height: 50%;
+          left: 0;
+          right: 0;
+          background: linear-gradient(transparent, #cccccc);
+        }
+      `}</style>
     </Layout>
   );
 }
