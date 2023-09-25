bcab54b186696021a68c9d670c09215783f82e06
bxxd <bxxd.eth@gmail.com>
2023-09-25 07:04:49 -0400

    update

--- app/composable/src/components/footer/Footer.tsx
+++ app/composable/src/components/footer/Footer.tsx
@@ -6,19 +6,17 @@ const Footer = () => {
   return (
     <footer className=" z-50 w-full border-t shadow-sm transition-all duration-300 opacity-85 bg-white text-black dark:border-gray-700 dark:bg-black-900 dark:text-gray-300">
       <div className="opacity-75 px-4 md:flex md:items-center md:justify-between py-1">
-        <div className="text-center md:text-left font-serif">
+        <div className="text-center md:text-left font-serif flex">
           Made with{" "}
           <Icon
-            icon="ph:heart-duotone"
-            width={18}
-            height={18}
-            color="rgba(255, 0, 0, 0.5)" // 50% transparent red
-            className="inline transition-colors"
+            icon="fluent-emoji-high-contrast:sweat-droplets"
+            color="#aaaa"
+            className="flex my-1 mx-1"
           />{" "}
           by{" "}
           <a
             href="https://twitter.com/bri4nr33d"
-            className="text-blue-500 hover:underline"
+            className="text-blue-500 hover:underline mx-1"
           >
             @bri4nr33d
           </a>
