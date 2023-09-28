1c9daf77fad74831cd1bbddf6285fb50e18dc228
bxxd <bxxd.eth@gmail.com>
2023-09-28 05:14:55 -0400

    update max-w

--- app/composable/src/components/workspace/workspace.tsx
+++ app/composable/src/components/workspace/workspace.tsx
@@ -59,13 +59,13 @@ export default function Workspace({}: WorkspaceProps) {
     // <div className="flex p-0 w-full border-gray-300 border border-dashed rounded-lg overflow-auto flex-grow h-full">
     <div className="flex p-0 w-full overflow-auto flex-grow h-full">
       {/* TipTap Component */}
-      <div className="flex flex-col w-full min-w-[36ch] max-w-none">
+      <div className="flex flex-col w-full min-w-[36ch] max-w-[130ch]">
         {showCatalog && <Catalog onToggleCatalog={handleToggleCatalog} />}
         <TipTap ref={tiptapRef} onToggleCatalog={handleToggleCatalog} />
       </div>
 
       {/* SearchColumn Component */}
-      <div className="flex-grow  min-w-[65ch] max-w-[65ch]">
+      <div className="flex-grow  min-w-[36ch] max-w-[65ch]">
         <SavedItems handleAddSaved={handleAddSaved} />
         <SearchColumn handleAddData={handleAddData} />
       </div>
