b658e6d647968c166f6436fd16bf6fc4e8c8d4c7
bxxd <bxxd.eth@gmail.com>
2023-11-06 18:03:26 -0500

    update metadata

--- app/composable/src/app/layout.tsx
+++ app/composable/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import "@/styles/globals.css";
 import type { Metadata } from "next";
+
 import { Inter } from "next/font/google";
 import { Analytics } from "@vercel/analytics/react";
 
@@ -8,6 +9,19 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Composable Parts",
   description: "Generative text with Composable Parts",
+  openGraph: {
+    title: "Composable Parts",
+    description: "Generative text with Composable Parts",
+    url: "https://composable.parts/",
+    siteName: "Composable Parts",
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    creator: "@bri4nr33d",
+    title: "Composable Parts",
+    description: "Generative text with Composable Parts",
+  },
 };
 
 export default function RootLayout({
